4a3a362ab876139a66cdded832fa1f841eb33a58
boisterous-media-9079 <luyao.mly@gmail.com>
2020-05-06 00:56:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === whisper(string)) {
+  if (string === string.toLowerCase()) {
     console.log("I can't hear you!");
-  } else if (string === shout(string)) {
+  } else if (string === string.toUpperCase()) {
     console.log("YES INDEED!");
   }
   if (string === "I love you, Grandma.") {
